228f3510f60035c362d6a8d54818eaa3f5ae1f6d
Mooniemd <doragondoshiro@gmail.com>
2023-08-21 13:23:32 -0400

    att do truco

--- js/truco.js
+++ js/truco.js
@@ -1,118 +1,45 @@
-let equipeA = {
-    nome: "",
-    pontos: 0,
-    vitorias: 0
-};
+let pontosTime1 = 0;
+let pontosTime2 = 0;
+let vitoriaTime1 = 0;
+let vitoriaTime2 = 0;
 
-let equipeB = {
-    nome: "",
-    pontos: 0,
-    vitorias: 0
-};
+function adicionaPonto(times, pontos) {
+    if (times === 'time1') {
+        pontosTime1 += pontos;
+        document.getElementById('pontosTime1').textContent = pontosTime1;
+    } else if (times === 'time2') {
+        pontosTime2 += pontos;
+        document.getElementById('pontosTime2').textContent = pontosTime2;
+    }
 
-const placarElement = document.getElementById("placar");
-const pontuacaoElement = document.getElementById("pontuacao");
-const equipeAInput = document.getElementById("equipeA");
-const equipeBInput = document.getElementById("equipeB");
-
-// att o placar
-function atualizarPlacar() {
-    placarElement.innerHTML = `
-        ${equipeA.nome} (${equipeA.vitorias} vitórias): ${equipeA.pontos} pontos<br>
-        ${equipeB.nome} (${equipeB.vitorias} vitórias): ${equipeB.pontos} pontos
-    `;
+    Vencedor();
 }
 
-// att a pontuação
-function atualizarPontuacao() {
-    pontuacaoElement.innerHTML = `
-        Pontuação Máxima: 12 pontos
-    `;
-}
-
-//inicia um novo jogo
-function novoJogo() {
-    equipeA.nome = equipeAInput.value;
-    equipeB.nome = equipeBInput.value;
-    equipeA.pontos = 0;
-    equipeB.pontos = 0;
-    atualizarPlacar();
-    atualizarPontuacao();
-}
-
-//  marcação de pontos da Equipe A
-document.getElementById("equipeAPonto1").addEventListener("click", function () {
-    if (equipeA.pontos < 12 && equipeB.pontos < 12) {
-        equipeA.pontos += 1;
-        verificarVitoria();
-    }
-    atualizarPlacar();
-});
-
-document.getElementById("equipeAPonto3").addEventListener("click", function () {
-    if (equipeA.pontos < 12 && equipeB.pontos < 12) {
-        equipeA.pontos += 3;
-        verificarVitoria();
-    }
-    atualizarPlacar();
-});
-
-document.getElementById("equipeAPonto6").addEventListener("click", function () {
-    if (equipeA.pontos < 12 && equipeB.pontos < 12) {
-        equipeA.pontos += 6;
-        verificarVitoria();
-    }
-    atualizarPlacar();
-});
-
-document.getElementById("equipeAPonto12").addEventListener("click", function () {
-    if (equipeA.pontos < 12 && equipeB.pontos < 12) {
-        equipeA.pontos += 12;
-        verificarVitoria();
-    }
-    atualizarPlacar();
-});
-
-// marcação de pontos da Equipe B
-document.getElementById("equipeBPonto1").addEventListener("click", function () {
-    if (equipeA.pontos < 12 && equipeB.pontos < 12) {
-        equipeB.pontos += 1;
-        verificarVitoria();
-    }
-    atualizarPlacar();
-});
-
-document.getElementById("equipeBPonto3").addEventListener("click", function () {
-    if (equipeA.pontos < 12 && equipeB.pontos < 12) {
-        equipeB.pontos += 3;
-        verificarVitoria();
-    }
-    atualizarPlacar();
-});
-
-document.getElementById("equipeBPonto6").addEventListener("click", function () {
-    if (equipeA.pontos < 12 && equipeB.pontos < 12) {
-        equipeB.pontos += 6;
-        verificarVitoria();
-    }
-    atualizarPlacar();
-});
-
-document.getElementById("equipeBPonto12").addEventListener("click", function () {
-    if (equipeA.pontos < 12 && equipeB.pontos < 12) {
-        equipeB.pontos += 12;
-        verificarVitoria();
-    }
-    atualizarPlacar();
-});
-
-//  verifica se uma equipe venceu
-function verificarVitoria() {
-    if (equipeA.pontos >= 12) {
-        equipeA.vitorias++;
-        novoJogo();
-    } else if (equipeB.pontos >= 12) {
-        equipeB.vitorias++;
-        novoJogo();
+function Vencedor() {
+    if (pontosTime1 >= 12) {
+        vitoriaTime1++;
+        document.getElementById('vitoriaTime1').textContent = vitoriaTime1;
+        resetaPontos();
+    } else if (pontosTime2 >= 12) {
+        vitoriaTime2++;
+        document.getElementById('vitoriaTime2').textContent = vitoriaTime2;
+        resetaPontos();
     }
 }
+
+function resetaPontos() {
+    pontosTime1 = 0;
+    pontosTime2 = 0;
+    document.getElementById('pontosTime1').textContent = pontosTime1;
+    document.getElementById('pontosTime2').textContent = pontosTime2;
+}
+
+function editaNomeDoTime(times) {
+    const novoNome = prompt(`Alterar nome ${times === 'time1' ? 'Time 1' : 'Time 2'}:`);
+    if (novoNome !== null && novoNome !== '') {
+        document.getElementById(`${times}-name`).textContent = novoNome;
+    }
+}
+
+document.getElementById('time1-name').textContent = prompt("Digite o nome do Time 1:");
+document.getElementById('time2-name').textContent = prompt("Digite o nome do Time 2:");
